ee900cc58c307a5e2f0247231d5968eee49da4b5
jaideepSingh100 <97889131+jaideepSingh100@users.noreply.github.com>
2022-05-09 09:43:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function output()
+        {
+            var count = "1 2 3 4 5 6 7 8 9";
+            
+            console.log(count);
+
+            var name_1 = "JAIDEEP SINGH";
+
+            console.log(name_1);
+
+            var name_2 = "SINGH";
+
+            console.log(name_2); 
+
+            var final_name = name_1, name_2;
+
+            console.log(final_name);
+        }
+            
+
